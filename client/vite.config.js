fe4f8e8c56ce7f821c16a667b1218e421f97a5eb
SUPRIYA YADAV <143317604+supriya0415@users.noreply.github.com>
2026-05-24 16:09:21 +0530

    Updated project

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    server:{
-      proxy:{
-        '/api':"http://localhost:5001"
-      }
-    },
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5001'
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
